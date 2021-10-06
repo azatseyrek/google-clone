189c58ee2782e5fe9c8f0336c894bdf21107ef78
azatseyrek <azatseyrek@hotmail.com>
2021-10-07 00:18:30 +0300

    googleApi connected to searchpage

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        {/* *Home (the one with the search on) */}
+        
+        {/* SearchPage (Result Page) */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
